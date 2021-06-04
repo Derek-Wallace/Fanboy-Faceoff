2537eaca325cade60d6ce5d7b49c235e1583612d
Derek-Wallace <lacrosseid@gmail.com>
2021-06-04 15:27:37 -0600

    i broke it

--- client/app/AppState.js
+++ client/app/AppState.js
@@ -7,10 +7,12 @@ class AppState extends EventEmitter {
   account = {}
   socketData = []
 
-  /**@type {Post[]} */
+  /** @type {Post[]} */
   posts = [{
     imgUrl: 'https://cdn.britannica.com/s:800x450,c:crop/26/187026-138-00A23A77/science-fiction-powers-Marvel-Comics-Avengers.jpg',
-    title: 'rip', voteCount: 4, id: '55555'
+    title: 'rip',
+    voteCount: 4,
+    id: '55555'
   }]
 }
 
