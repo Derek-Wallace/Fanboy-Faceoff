aaf05aecf2fa1b0cbd712a346b95ccab2976e281
hannah hayward <hannahhayward97@gmail.com>
2021-06-04 18:59:42 -0600

    votes done

--- client/app/Controllers/PostsController.js
+++ client/app/Controllers/PostsController.js
@@ -12,12 +12,37 @@ function draw() {
       <img src="${p.imgUrl}" alt="" style="object-fit: contain;"></div>
       <p class = "text-right">
       <h2>${p.title}</h2> 
-      <button class="btn text-right"><i class="fas fa-lg fa-arrow-up text-primary" onclick="app.postsController.upVote('${p.id}')"></i></button> <button class="btn "><i class="fas fa-lg fa-arrow-down text-danger" onclick="app.postsController.downVote('${p.id}')"></i></button></p>
-      <p class= "text-right" id="${p.id}">${p.voteCount}</p>
+      <button class="btn text-right"><i class="fas fa-lg fa-arrow-up text-primary" onclick="app.postsController.vote('${p.id}', 'upvote')"></i></button> <button class="btn "><i class="fas fa-lg fa-arrow-down text-danger" onclick="app.postsController.vote('${p.id}', 'downvote')"></i></button></p>
+      <p id="${p.id}"></p>
     </div>
   </div>`
   })
   document.getElementById('posts').innerHTML = template
+}
+
+function drawIcon(id) {
+  const post = ProxyState.posts.find(p => p.id === id)
+  if (post.voteCount > -5 && post.voteCount < 5) {
+    document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-user-tie"> ${post.voteCount}</i></p>`
+  }
+  if (post.voteCount >= 5 && post.voteCount < 10) {
+    document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-mask"> ${post.voteCount}</i></p>`
+  }
+  if (post.voteCount >= 10 && post.voteCount < 15) {
+    document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-meteor"> ${post.voteCount}</i></p>`
+  }
+  if (post.voteCount >= 15) {
+    document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-dna"> ${post.voteCount}</i></p>`
+  }
+  if (post.voteCount <= -5 && post.voteCount > -10) {
+    document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-trash-alt"> ${post.voteCount}</i></p>`
+  }
+  if (post.voteCount <= -10 && post.voteCount > -15) {
+    document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-dumpster"> ${post.voteCount}</i></p>`
+  }
+  if (post.voteCount <= -15) {
+    document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-dumpster-fire"> ${post.voteCount}</i></p>`
+  }
 }
 
 export class PostsController {
@@ -31,35 +56,13 @@ export class PostsController {
     draw()
   }
 
-  upVote(id) {
+  vote(id, choice) {
     const post = ProxyState.posts.find(p => p.id === id)
-    post.voteCount += 1
-    console.log('button worked', post.voteCount)
-    draw()
-    // document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-user-tie"> ${post.voteCount}</i></p>`
-    if (post.voteCount >= 5) {
-      document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-mask"> ${post.voteCount}</i></p>`
-    } if (post.voteCount >= 10) {
-      document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-meteor"> ${post.voteCount}</i></p>`
-    } if (post.voteCount >= 15) {
-      document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-superpowers"> ${post.voteCount}</i></p>`
+    if (choice === 'upvote') {
+      post.voteCount += 1
+    } else if (choice === 'downvote') {
+      post.voteCount -= 1
     }
-  }
-
-  downVote(id) {
-    const post = ProxyState.posts.find(p => p.id === id)
-    post.voteCount -= 1
-    console.log('button worked', post.voteCount)
-    draw()
-    if (post.voteCount <= -5) {
-      document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-trash-alt"> ${post.voteCount}</i></p>`
-    }
-    if (post.voteCount <= -10) {
-      document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-dumpster"> ${post.voteCount}</i></p>`
-    }
-    if (post.voteCount <= -15) {
-      document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-dumpster-fire"> ${post.voteCount}</i></p>`
-    }
-    // document.getElementById(id).innerHTML = /* html */ `<p class= "text-right" id="${post.id}"> <i class="fas fa-lg fa-user-tie"> ${post.voteCount}</i></p>`
+    drawIcon(id)
   }
 }
--- client/app/Services/PostsService.js
+++ client/app/Services/PostsService.js
@@ -1,18 +1,17 @@
-import { logger } from '../Utils/Logger.js'
+// import { logger } from '../Utils/Logger.js'
 
-// eslint-disable-next-line no-undef
-const url = axios.create({
-  baseURL: 'http://localhost:3000/'
-})
+// const url = axios.create({
+//   baseURL: 'http://localhost:3000/'
+// })
 class PostsService {
   async getPosts() {
-    const res = await url.get('posts')
-    logger.log(res.data)
+    // const res = await url.get('posts')
+    // logger.log(res.data)
   }
 
   async createPost(formData) {
-    const res = await url.post('posts', formData)
-    logger.log(res.data)
+    // const res = await url.post('posts', formData)
+    // logger.log(res.data)
   }
 }
 export const postsService = new PostsService()
