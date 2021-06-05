7bba221b9a78f0059b8a6d924eef9d2944660071
Derek-Wallace <lacrosseid@gmail.com>
2021-06-04 19:16:19 -0600

    yayayay

--- client/app/Services/PostsService.js
+++ client/app/Services/PostsService.js
@@ -3,12 +3,12 @@ import { api } from './AxiosService.js'
 
 class PostsService {
   async getPosts() {
-    const res = await api.get('posts')
+    const res = await api.get('api/posts')
     ProxyState.posts = res.data
   }
 
   async createPost(formData) {
-    const res = await api.post('posts', formData)
+    const res = await api.post('api/posts', formData)
     ProxyState.posts.push(res.data)
     // eslint-disable-next-line no-self-assign
     ProxyState.posts = ProxyState.posts
