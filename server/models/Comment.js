ce1f171a9544bf365d9f0ee7edf20a35cf5df06c
Derek-Wallace <lacrosseid@gmail.com>
2021-06-06 18:15:17 -0600

    starting votecount

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 export const CommentSchema = new mongoose.Schema({
   content: { type: String, required: true },
-  commentVoteCount: { type: Number },
+  commentVoteCount: { type: Number, default: 0 },
   postId: { type: Schema.Types.ObjectId, ref: 'postId', required: true }
 },
 { timestamps: true, toJSON: { virtuals: true } })
--- server/models/Post.js
+++ server/models/Post.js
@@ -5,7 +5,7 @@ export const PostSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String },
   imgUrl: { type: String },
-  voteCount: { type: Number }
+  voteCount: { type: Number, default: 0 }
 
 },
 { timestamps: true, toJSON: { virtuals: true } }
